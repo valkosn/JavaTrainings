12f2a387ab833e15de31b2fdbb8e9576f41fb2a7
Sergio <valkosn1987@gmail.com>
2015-06-24 22:11:03 +0300

    Done.

---
+++ src/com/javarush/test/level07/lesson06/task05/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.test.level07.lesson06.task05;
+
+/* Удали последнюю строку и вставь её в начало
+1. Создай список строк.
+2. Добавь в него 5 строчек с клавиатуры.
+3. Удали последнюю строку и вставь её в начало. Повторить 13 раз.
+4. Используя цикл выведи содержимое на экран, каждое значение с новой строки.
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        ArrayList<String> strings = new ArrayList<String>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int i = 0; i < 5; i++)
+        {
+            strings.add(reader.readLine());
+        }
+
+        for (int i = 0; i < 13; i++)
+        {
+            String temp = strings.get(4);
+            strings.remove(4);
+            strings.add(0,temp);
+        }
+
+        for (String counter : strings) {
+
+            System.out.println(counter);
+        }
+
+
+
+
+    }
+}
