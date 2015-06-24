2e3573c9a629571e3984065707bab43b72674d5b
Sergio <valkosn1987@gmail.com>
2015-06-24 22:03:06 +0300

    Done.

---
+++ src/com/javarush/test/level07/lesson06/task04/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.test.level07.lesson06.task04;
+
+/* 5 строчек в начало списка
+1. Создай список строк.
+2. Добавь в него 5 строчек с клавиатуры, но только добавлять не в конец списка, а в начало.
+3. Используя цикл выведи содержимое на экран, каждое значение с новой строки.
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        ArrayList<String> strings = new ArrayList<String>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int i = 0; i < 5; i++)
+        {
+            strings.add(0,reader.readLine());
+
+        }
+
+        for (int i = 0; i < strings.size(); i++)
+        {
+            System.out.println(strings.get(i));
+        }
+
+
+    }
+}
