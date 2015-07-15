65daa815f0f3efc1f43a62b0f1ca3e1efaf2cc14
valkosn <valkosn1987@gmail.com>
2015-07-15 17:24:06 +0300

    something like this...

---
+++ src/Orest/test/fibonachi/test1.java
@@ -0,0 +1,49 @@
+package Orest.test.fibonachi;
+
+/**
+ * Created by vsn on 15.07.15.
+ */
+public class test1
+{
+    public static void main(String[] args)
+    {
+        System.out.println(fibonachi(0));
+        System.out.println(fibonachi(1));
+        System.out.println(fibonachi(5));
+        System.out.println(fibonachi(-5));
+        System.out.println(fibonachi(16));
+        System.out.println(fibonachi(-16));
+    }
+
+    public static long fibonachi(int n)
+    {
+        if (n >= 0)
+        {
+            switch (n)
+            {
+                case 0:
+                    return 0;
+                case 1:
+                    return 1;
+                default:
+                    return fibonachiNumbers(n);
+            }
+        } else
+        {
+            return Math.abs(n) == 1 ? -1 : 0 - fibonachiNumbers(Math.abs(n));
+        }
+    }
+
+    private static long fibonachiNumbers(int n)
+    {
+        long[] temp = new long[n];
+        temp[0] = 0;
+        temp[1] = 1;
+        for (int i = 2; i <= temp.length - 1; i++)
+        {
+            temp[i] = temp[i - 1] + temp[i - 2];
+        }
+        return temp[n - 1];
+    }
+
+}
