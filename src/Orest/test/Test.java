458b626a7885bad77aea380a2ae1bd0e16430464
Orest <orecto@gmail.com>
2015-07-20 22:42:01 +0300

    Crap

---
+++ src/Orest/test/Test.java
@@ -0,0 +1,13 @@
+package Orest.test;
+
+/**
+ * @author OrestO
+ * @since 7/20/2015
+ */
+public class Test
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Hello world");
+  }
+}
