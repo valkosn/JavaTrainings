7291ed0ce3171d5ecd55208097bf218fda7d47ad
Sergio <valkosn1987@gmail.com>
2015-07-23 00:05:37 +0300

    fix superFibCycleImpl method and add  superFibRecImpl method(in progress)

--- src/Orest/test/SuperFibonacci.java
+++ src/Orest/test/SuperFibonacci.java
@@ -1,14 +1,25 @@
 package Orest.test;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 /**
  * Created by Ra1004ek on 16.07.2015.
  */
 public class SuperFibonacci
 {
+    final static String mIncorrect = "Value m is incorrect, must be >=2";
 
-    public long superFibArrayImpl(int n, int m)
+    public static void main(String[] args)
+    {
+        System.out.println(superFibCycleImpl(9, 4));
+        System.out.println(superFibCycleImpl(7, 3));
+        System.out.println(superFibCycleImpl(9, 5));
+        System.out.println(superFibCycleImpl(2, 4));
+        System.out.println(superFibCycleImpl(1, 1));
+    }
+
+    public static long superFibArrayImpl(int n, int m)
     {
 
         ArrayList<Long> result = new ArrayList<>();
@@ -31,35 +42,61 @@ public class SuperFibonacci
             }
 
 
-        } else System.out.println("Value m is incorrect, must be >=2");
+        } else System.out.println(mIncorrect);
 
 
         return result.get(n - 1);
     }
 
-    /*public long superFibCycleImpl(int n, int m)
+    public static long superFibCycleImpl(int n, int m)
     {
         long result = 0;
         if (m >= 2)
         {
-            long firstSum = 1;
-            for (int i = 0; i < m; i++)
+            if (m >= n) return 1;
+            else
             {
-                firstSum += 1;
-            }
-            for (int i = m; i < n; i++)
-            {
-                long temp1 = firstSum;
-                long temp2 =
+                LinkedList<Long> list = new LinkedList<>();
+                for (int i = 0; i < m; i++)
+                {
+                    list.add(1L);
+                }
+
+                for (int i = m; i < n; i++)
+                {
+                    result = 0;
+                    for (Long l : list) result += l;
+
+                    list.removeFirst();
+                    list.add(result);
+
+                }
 
             }
 
 
-        } else System.out.println("Value m is incorrect, must be >=2");
+        } else System.out.println(mIncorrect);
 
 
         return result;
 
-    }*/
+    }
 
+    public static long superFibRecImpl(int n, int m)
+
+    {
+        if (m >= 2)
+        {
+            if (m >= n) return 1;
+            else
+            {
+
+            }
+
+        } else System.out.println(mIncorrect);
+
+        return 0;
+    }
 }
+
+
