6e3064bf298241532e231ec678dc6b26f6e6e00f
Sergio <valkosn1987@gmail.com>
2015-08-16 18:34:22 +0300

    Done

---
+++ src/com/javarush/test/level12/lesson04/task02/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.test.level12.lesson04.task02;
+
+/* print(int) и print(Integer)
+Написать два метода: print(int) и print(Integer).
+Написать такой код в методе main, чтобы вызвались они оба.
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        int a = 5;
+        Integer b = 7;
+        print(a);
+        print(b);
+
+    }
+
+    public static void print(int a)
+    {
+
+    }
+
+    public static void print(Integer a)
+    {
+
+    }
+
+    //Напишите тут ваши методы
+}
