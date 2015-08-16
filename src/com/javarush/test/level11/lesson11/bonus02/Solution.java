5882aa21f71cf08c03afd9a51c39d58a2e2b5cbf
Sergio <valkosn1987@gmail.com>
2015-08-16 16:35:23 +0300

    Done

---
+++ src/com/javarush/test/level11/lesson11/bonus02/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.test.level11.lesson11.bonus02;
+
+/* Нужно добавить в программу новую функциональность
+Добавь общий базовый класс к классам-фигур:  (фигуры из шахмат).
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+    }
+
+    public class ChessItems
+    {
+    }
+
+    public class King extends ChessItems
+    {
+    }
+
+    public class Queen extends ChessItems
+    {
+    }
+
+    public class Rook extends ChessItems
+    {
+    }
+
+    public class Knight extends ChessItems
+    {
+    }
+
+    public class Bishop extends ChessItems
+    {
+    }
+
+    public class Pawn extends ChessItems
+    {
+    }
+}
