76cae0e2984b407b10bea95e3a27bea971e52ff9
Sergio <valkosn1987@gmail.com>
2015-09-04 01:29:23 +0300

    done

--- src/com/javarush/test/level19/lesson08/task04/Solution.java
+++ src/com/javarush/test/level19/lesson08/task04/Solution.java
@@ -14,16 +14,58 @@ package com.javarush.test.level19.lesson08.task04;
 3 + 6 = 9
 */
 
-public class Solution {
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+public class Solution
+{
     public static TestString testString = new TestString();
 
-    public static void main(String[] args) {
-    }
+    public static void main(String[] args) throws IOException
+    {
 
-    public static class TestString {
-        public void printSomething() {
-            System.out.println("3 + 6 = ");
+        PrintStream printStream = System.out;
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream stream = new PrintStream(outputStream);
+        System.setOut(stream);
+        testString.printSomething();
+        System.setOut(printStream);
+
+        String[] firstResult = outputStream.toString().split(" ");
+        outputStream.close();
+        stream.close();
+        int a = Integer.parseInt(firstResult[0]);
+        int b = Integer.parseInt(firstResult[2]);
+
+        switch (firstResult[1])
+        {
+            case "+":
+            {
+                System.out.println(a + " + " + b + " = " + (a+b));
+            }
+
+            break;
+            case "-":
+            {
+                System.out.println(a + " - " + b + " = " + (a-b));
+            }
+            break;
+            case "*":
+            {
+                System.out.println(a + " * " + b + " = " + (a*b));
+            }
+        }
+
+        }
+
+        public static class TestString
+        {
+            public void printSomething()
+            {
+                System.out.println("3 + 6 = ");
+            }
         }
     }
-}
 
