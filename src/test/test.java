436d09e5d388e4827ac4a4ebdc406df7a3a2ed56
valkosn <valkosn1987@gmail.com>
2015-11-26 01:56:31 +0200

    Date parser with magic numbers

--- src/test/test.java
+++ src/test/test.java
@@ -1,25 +1,12 @@
 package test;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 
-/**
- * Created by vsn on 03.09.15.
- */
 public class test
 {
-    public static void main(String[] args) throws Exception
-    {
-
-        String str = "dfkgljdhfg 65324 dfgsdfg4654 321321вапыва ывап цукен вапывапЫВАПЫВП";
 
 
-        char[] arrStr = str.toCharArray();
-        BufferedWriter writer = new BufferedWriter(new FileWriter("d:\\test11.txt"));
 
 
-        writer.write(arrStr, 0, arrStr.length);
-        writer.close();
-
-    }
 }
+
+
