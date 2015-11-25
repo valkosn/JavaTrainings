59ef1f529998af16fbd912d60713dd3b7c25ab0b
valkosn <valkosn1987@gmail.com>
2015-11-26 01:53:32 +0200

    Date parser with magic numbers

---
+++ src/regex/DateParser.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2015.
+ */
+
+package regex;
+
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by Valko Serhii on 26.11.2015.
+ */
+
+public class DateParser
+{
+    public static void main(String[] args) throws IOException
+    {
+        String year = "Year - ";
+        String month = "Month - ";
+        String day = "Day - ";
+
+        String data = "2015.111.26";
+
+        Pattern pattern = Pattern.compile("^([12]\\d{3})\\.((0[1-9])|(1[0-2]))\\.((0[1-9])|([12][0-9])|(3[0-1]))");
+        Matcher matcher = pattern.matcher(data);
+
+        matcher.find();
+        try
+        {
+            year += matcher.group(1);
+            month += matcher.group(2);
+            day += matcher.group(5);
+            System.out.println(year + "\n" + month + "\n" + day);
+
+        }
+        catch (IllegalStateException e)
+        {
+            System.out.println("Ops... I catch exception!");
+        }
+
+    }
+}
