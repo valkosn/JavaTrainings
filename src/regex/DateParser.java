d90c345b8e8c13f07fd25663ce861006b8b2681c
Valko Serhii <valkosn1987@gmail.com>
2015-11-26 11:59:52 +0200

    again refactoring

--- src/regex/DateParser.java
+++ src/regex/DateParser.java
@@ -12,28 +12,37 @@ import java.util.regex.Pattern;
  * Created by Valko Serhii on 26.11.2015.
  */
 
-public class DateParser
-{
+public class DateParser {
     private static String year = "Year - ";
     private static String month = "Month - ";
     private static String day = "Day - ";
 
-    public static void main(String[] args) throws IOException
-    {
-        Pattern pattern1 = Pattern.compile("^([12]\\d{3})\\.((0[1-9])|(1[0-2]))\\.((0[1-9])|([12][0-9])|(3[0-1]))");
+    public static void main(String[] args) throws IOException {
+        String data = "2015.11.26";
 
-        parseDate(pattern1, "2015.11.26");
+        parseDate(initPattern(), data);
 
     }
 
-    public static void parseDate(Pattern pattern, String data)
-    {
+    public static Pattern initPattern() {
+        StringBuilder commonPattern = new StringBuilder();
+        commonPattern.append("^((19[7-9]\\d)|(20[01]\\d))"); // year regex
+        commonPattern.append("\\.((0[1-9])|(1[0-2]))"); // month regex
+        commonPattern.append("\\.((0[1-9])|([12][0-9])|(3[0-1]))"); // day regex
+
+        Pattern pattern = Pattern.compile(commonPattern.toString());
+
+        return pattern;
+
+    }
+
+
+    public static void parseDate(Pattern pattern, String data) {
         Matcher matcher = pattern.matcher(data);
-        if (matcher.find())
-        {
-            year += matcher.group(1);
-            month += matcher.group(2);
-            day += matcher.group(5);
+        if (matcher.find()) {
+            year += matcher.group(1); // magic
+            month += matcher.group(4);  // magic
+            day += matcher.group(7);  // magic
             System.out.println(year + "\n" + month + "\n" + day);
 
         } else
