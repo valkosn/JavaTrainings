1ccd51bb500b543c9f2efd47eea7a852ac29d4e4
valkosn <valkosn1987@gmail.com>
2015-12-01 01:24:15 +0200

    done

--- src/com/javarush/test/level18/lesson10/home09/Solution.java
+++ src/com/javarush/test/level18/lesson10/home09/Solution.java
@@ -1,5 +1,7 @@
 package com.javarush.test.level18.lesson10.home09;
 
+import java.io.*;
+
 /* Файлы и исключения
 Читайте с консоли имена файлов
 Если файла не существует (передано неправильное имя файла), то
@@ -7,8 +9,39 @@ package com.javarush.test.level18.lesson10.home09;
 Закрыть потоки. Не использовать try-with-resources
 Не используйте System.exit();
 */
-//TODO:new
-public class Solution {
-    public static void main(String[] args) {
+//TODO:done...
+public class Solution
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        while (true)
+        {
+
+            String filePath = reader.readLine();
+            try
+            {
+                readFromConsole(filePath);
+            }
+            catch (FileNotFoundException e)
+            {
+                System.out.println(filePath);
+                break;
+            }
+            catch (IOException e)
+            {
+
+            }
+
+        }
+        reader.close();
+    }
+
+    public static void readFromConsole(String filePath) throws IOException
+    {
+        FileInputStream inputStream = new FileInputStream(filePath);
+        inputStream.close();
+
+
     }
 }
