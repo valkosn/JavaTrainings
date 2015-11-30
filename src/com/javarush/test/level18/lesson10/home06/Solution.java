d47c2dd9683884bb1e5bd2b2fc7ae780485e4073
valkosn <valkosn1987@gmail.com>
2015-12-01 01:24:04 +0200

    done

--- src/com/javarush/test/level18/lesson10/home06/Solution.java
+++ src/com/javarush/test/level18/lesson10/home06/Solution.java
@@ -14,9 +14,38 @@ package com.javarush.test.level18.lesson10.home06;
 - 7
 f 361
 */
+//TODO:done
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Map;
+import java.util.TreeMap;
 
-public class Solution {
-    public static void main(String[] args) {
+public class Solution
+{
+    public static void main(String[] args) throws IOException
+    {
+
+        FileInputStream inputStream = new FileInputStream(args[0]);
+        Map<Integer, Integer> resMap = new TreeMap<>();
+
+        while (inputStream.available() > 0)
+        {
+            Integer tempChar = inputStream.read();
+            Integer tempCharCount = resMap.get(tempChar);
+            if (tempCharCount == null)
+                resMap.put(tempChar, 1);
+            else
+            {
+                resMap.put(tempChar, tempCharCount + 1);
+            }
+        }
+
+        inputStream.close();
+
+        for (int key : resMap.keySet())
+        {
+            System.out.println((char) key + " " + resMap.get(key));
+        }
 
     }
 }
