2c0dc46e929de30c68fa7a2889700944353fc09d
valkosn <valkosn1987@gmail.com>
2015-12-01 01:24:39 +0200

    done

--- src/com/javarush/test/level18/lesson10/home08/Solution.java
+++ src/com/javarush/test/level18/lesson10/home08/Solution.java
@@ -1,7 +1,7 @@
 package com.javarush.test.level18.lesson10.home08;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.*;
+import java.util.*;
 
 /* Нити и байты
 Читайте с консоли имена файлов, пока не будет введено слово "exit"
@@ -10,18 +10,86 @@ import java.util.Map;
 где параметр String - это имя файла, параметр Integer - это искомый байт.
 Закрыть потоки. Не использовать try-with-resources
 */
-//TODO:new
-public class Solution {
+//TODO:done
+/*
+
+E:\Work\test\test1.txt
+E:\Work\test\test2.txt
+E:\Work\test\test3.txt
+E:\Work\test\test4.txt
+exit
+
+*/
+
+public class Solution
+{
     public static Map<String, Integer> resultMap = new HashMap<String, Integer>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true)
+        {
+            String fileName = reader.readLine();
+            if (fileName.equals("exit")) break;
+            new ReadThread(fileName).start();
+        }
+        reader.close();
 
     }
 
-    public static class ReadThread extends Thread {
-        public ReadThread(String fileName) {
+
+    public static class ReadThread extends Thread
+    {
+
+        String fileName;
+
+        public ReadThread(String fileName) throws Exception
+        {
             //implement constructor body
+            this.fileName = fileName;
         }
+
         // implement file reading here - реализуйте чтение из файла тут
+
+
+        public void run()
+        {
+            Map<Integer, Integer> tempMap = new HashMap<>();
+            Integer maxByteByFrequency = null;
+
+            try
+            {
+                FileInputStream fileInputStream = new FileInputStream(fileName);
+
+                while (fileInputStream.available() > 0)
+                {
+                    Integer currByte = fileInputStream.read();
+                    Integer currByteCount = tempMap.get(currByte);
+
+                    if (currByteCount == null)
+                        tempMap.put(currByte, 1);
+                    else tempMap.put(currByte, ++currByteCount);
+                }
+                fileInputStream.close();
+            }
+            catch (IOException e)
+            {
+            }
+
+            for (Integer key : tempMap.keySet())
+            {
+
+                int maxCount = 0;
+                if (tempMap.get(key) > maxCount) maxByteByFrequency = key;
+            }
+
+            resultMap.put(fileName, maxByteByFrequency);
+
+
+        }
+
+
     }
 }
