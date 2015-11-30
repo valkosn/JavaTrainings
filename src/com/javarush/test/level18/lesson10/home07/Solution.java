c379e29b6817b8575ebfd4a2ff9bd501271c50bb
valkosn <valkosn1987@gmail.com>
2015-12-01 01:23:45 +0200

    done

--- src/com/javarush/test/level18/lesson10/home07/Solution.java
+++ src/com/javarush/test/level18/lesson10/home07/Solution.java
@@ -1,5 +1,7 @@
 package com.javarush.test.level18.lesson10.home07;
 
+import java.io.*;
+
 /* Поиск данных внутри файла
 Считать с консоли имя файла
 Найти в файле информацию, которая относится к заданному id, и вывести ее на экран в виде, в котором она записана в файле.
@@ -16,8 +18,27 @@ quantity - количество, int
 
 Информация по каждому товару хранится в отдельной строке
 */
-//TODO:new
+//TODO:done
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException
+    {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String filePath = reader.readLine();
+
+        BufferedReader inputStream = new BufferedReader(new FileReader(filePath));
+
+        while(inputStream.ready()){
+
+            String line = inputStream.readLine();
+            String id = line.split(" ")[0];
+
+            if (id.equals(args[0])) {
+                System.out.println(line);
+            }
+        }
+        reader.close();
+        inputStream.close();
+
     }
 }
