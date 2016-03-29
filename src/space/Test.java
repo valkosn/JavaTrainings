3e7310a32622d2ef6cd86211b9350fdc32ebf284
oresyto <30a0e501ee3849e196468db2133eb3d5>
2016-03-30 00:45:50 +0300

    Test: Add CrUD multi file implementation.

--- src/space/Test.java
+++ src/space/Test.java
@@ -11,6 +11,6 @@ public class Test
 {
     public static void main(String[] args)
     {
-        System.out.println(SolarSystem.MOON.getHead().getRadius());
+//        System.out.println(SolarSystem.MOON.getHead().getRadius());
     }
 }
